5107aa14bdaef8de19e2fa5533752776ff6b2a82
Sean Polyn <sean.polyn@vanderbilt.edu>
2022-08-09 16:36:36 -0500

    updated bootstrap ci to use mean with omitnan option

--- utils/bootstrap_ci.m
+++ utils/bootstrap_ci.m
@@ -4,7 +4,7 @@ function ci = bootstrap_ci(x, n_perm)
 m = NaN(n_perm, n_cond);
 for i = 1:n_perm
   ind = randsample(size(x, 1), size(x, 1), true);
-  m(i,:) = nanmean(x(ind,:), 1);
+  m(i,:) = mean(x(ind,:), 1,'omitnan');
 end
 
 ci = prctile(m, [2.5 97.5], 1);
